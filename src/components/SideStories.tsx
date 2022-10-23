58e4c1cdcfe6c8de66f9f45f37608eafdc273a86
Stephen Moran <stephenmoran@live.ie>
2022-10-23 19:29:25 +0100

    Update SideStories.tsx

--- src/components/SideStories.tsx
+++ src/components/SideStories.tsx
@@ -1,11 +1,75 @@
 import React from "react";
 
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 const SideStories = () => {
   return (
-    <Box h="512px" backgroundColor="red.400">
-      SideStories
+    <Box pt="32px" pr="24px" pl="24px" h="512px" backgroundColor="#00001A">
+      <Text
+        as="h2"
+        fontSize="40px"
+        lineHeight="40px"
+        fontWeight="bold"
+        color="#E9AA52"
+        mb="34px"
+      >
+        New
+      </Text>
+      <Box mt="0" borderBottom="1px solid #5E607A">
+        <Text as="h3" color="#FFFDFA" fontSize="20px" fontWeight="extrabold">
+          Hydrogen VS Electric Cars
+        </Text>
+        <Text
+          as="p"
+          color="#C5C6CE"
+          fontSize="15px"
+          lineHeight="26px"
+          fontWeight="normal"
+          mt="5px"
+          mb="29px"
+        >
+          Will hydrogen-fueled cars ever catch up to EVs?
+        </Text>
+      </Box>
+
+      <Box mt="0" borderBottom="1px solid #5E607A" mb="29px">
+        <Text
+          as="h3"
+          color="#FFFDFA"
+          fontSize="20px"
+          fontWeight="extrabold"
+          mt="31px"
+        >
+          Hydrogen VS Electric Cars
+        </Text>
+        <Text
+          as="p"
+          color="#C5C6CE"
+          fontSize="15px"
+          lineHeight="26px"
+          fontWeight="normal"
+          mt="5px"
+          mb="29px"
+        >
+          Will hydrogen-fueled cars ever catch up to EVs?
+        </Text>
+      </Box>
+
+      <Box mt="0">
+        <Text as="h3" color="#FFFDFA" fontSize="20px" fontWeight="extrabold">
+          Hydrogen VS Electric Cars
+        </Text>
+        <Text
+          as="p"
+          color="#C5C6CE"
+          fontSize="15px"
+          lineHeight="26px"
+          fontWeight="normal"
+          mt="5px"
+        >
+          Will hydrogen-fueled cars ever catch up to EVs?
+        </Text>
+      </Box>
     </Box>
   );
 };
