25a5ed84c510ef04deee50caa8a9492f170d94eb
Stephen Moran <stephenmoran@live.ie>
2022-11-02 18:33:54 +0000

    updated app.tsx file

--- news-homepage-1/src/App.tsx
+++ news-homepage-1/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import "./App.css";
-
 import { Box } from "@chakra-ui/react";
+
+import "./App.css";
 
 import Header from "./components/Header";
 import MainStory from "./components/MainStory";
